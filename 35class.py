ae23866cb022a7ccdf8429c5adad3e00bf23b773
Iso Takuma <tak@Iso-no-MacBook-Air.local>
2018-10-02 22:39:21 +0530

    class

---
+++ 35class.py
@@ -0,0 +1,17 @@
+class Family :
+    def __init__( self ,name, address):
+        self.name = name 
+        self.address = address
+
+brother = Family ("masaki" , "kanagawa")
+father = Family ("kazuyoshi" , "ibaraki")
+
+print(brother)
+print(father)
+
+print(brother.name)
+print(father.name)
+
+
+
+
