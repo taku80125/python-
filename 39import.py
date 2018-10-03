db34b33ec4f81acceacf325bbf6031a1faddef0d
Iso Takuma <tak@Iso-no-MacBook-Air.local>
2018-10-03 12:43:04 +0530

    import

---
+++ 39import.py
@@ -0,0 +1,5 @@
+from classes import friend
+
+f1 = friend("kokoro",22,"nara")
+print(f1.name)
+print(friend.add_friend)
---
+++ classes.py
@@ -0,0 +1,9 @@
+class friend:
+    add_friend = 0
+
+    def __init__(self,name,age,uni):
+        self.name = name
+        self.age = age
+        self.uni = uni
+    
+        friend.add_friend +=1
