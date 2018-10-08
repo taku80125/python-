47801f27358bd9c6dcaad2978c88fc293b69d3dd
Iso Takuma <tak@Iso-no-MacBook-Air.local>
2018-10-08 15:17:26 +0530

    1.2

---
+++ 1.py
@@ -0,0 +1,4 @@
+name = input('whats your name')
+age = input('how old are you')
+
+print('you are',name,'and you are',age,'years old')
---
+++ 2.py
@@ -0,0 +1,6 @@
+a = input("number")
+b = input("bumber2")
+
+area = 3.14 * int(a) * int(b)
+
+print(area)
